63c97aa2284ea43acf4be9fda36a62fcc0a4e467
Ricardo Poleto <rprojetos.ti@hotmail.com>
2021-06-14 02:22:33 -0300

    Melhoria na estilização com bootstrap

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
+    'bootstrapform',
     'home',
     'clientes',
 ]
@@ -124,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    'statics',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
